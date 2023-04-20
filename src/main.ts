b46e857d66594d586db795db373da623fc59bea8
三生 <496226984@qq.com>
2023-04-20 10:47:47 +0800

    main 增加分析能力

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import { setupI18n } from './locales'
 import { setupAssets, setupNaive, setupScrollbarStyle } from './plugins'
@@ -9,6 +11,7 @@ async function bootstrap() {
   const app = createApp(App)
   setupAssets()
 
+  inject()
   setupScrollbarStyle()
 
   setupNaive(app)

5762384267ec4f041d7f9436136bce4d79760bd2
三生 <496226984@qq.com>
2023-04-20 10:32:52 +0800

    请求使用 Vercel Function

---
+++ src/api/fetchChat.ts
@@ -0,0 +1,33 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const apiKey = process.env.VITE_GLOB_OPENAI_API_KEY
+  const apiUrl = 'https://api.openai.com/v1/chat/completions'
+
+  try {
+    const requestBody = req.body
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    })
+
+    if (!response.ok) {
+      const errorResponse = await response.json()
+      const errorMessage = errorResponse.error?.message || 'An unknown error occurred.'
+      res.status(response.status).json({ error: errorMessage })
+      return
+    }
+
+    const responseData = await response.json()
+    res.status(200).json(responseData)
+  }
+  catch (error) {
+    console.error('Error while processing request:', error)
+    res.status(500).json({ error: 'An error occurred while processing the request.' })
+  }
+}

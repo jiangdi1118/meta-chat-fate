b599ba5a30afbacfa596355c90632ec67461f84c
三生 <496226984@qq.com>
2023-04-20 11:26:48 +0800

    Vercel Function 验证

--- api/fetchChat.ts
+++ api/fetchChat.ts
@@ -1,3 +1,4 @@
+// Content-Type: application/typescript
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
